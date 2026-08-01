c622ce87fd47f4f0d24a045d2448384bacf4ba6c
Sadiya. <sadiya.cse12@gmail.com>
2026-08-01 16:06:02 +0530

    Update auth.py

--- ai-powered-insider-threat-detection-system/dashboard/auth.py
+++ ai-powered-insider-threat-detection-system/dashboard/auth.py
@@ -2,7 +2,6 @@ import bcrypt
 from database import get_connection
 
 def login(username, password):
-
     conn = get_connection()
     cursor = conn.cursor()
 
@@ -12,42 +11,12 @@ def login(username, password):
     )
 
     row = cursor.fetchone()
-
     conn.close()
 
     if row is None:
         return False, None
 
     user = dict(row)
-
-    if bcrypt.checkpw(
-        password.encode(),
-        user["password"].encode()
-    ):
-        return True, user
-
-    return False, None
-from database import get_connection
-
-def login(username, password):
-
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute(
-    "SELECT * FROM users WHERE username=?",
-    (username,)
-)
-
-   row = cursor.fetchone()
-
-if row:
-    user = dict(row)
-
-    conn.close()
-
-    if user is None:
-        return False, None
 
     if bcrypt.checkpw(
         password.encode(),
