181d34e2abf6348630aa10265d171f1685a40670
Sadiya. <sadiya.cse12@gmail.com>
2026-08-01 16:52:22 +0530

    Modify database connection to use BASE_DIR
    Updated database connection to use dynamic path.

--- ai-powered-insider-threat-detection-system/dashboard/database.py
+++ ai-powered-insider-threat-detection-system/dashboard/database.py
@@ -1,8 +1,13 @@
 import sqlite3
+import os
 
-DB_NAME = "insider_threat.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "insider_threat.db")
+
+print("DATABASE PATH:", DB_PATH)
+print("DATABASE EXISTS:", os.path.exists(DB_PATH))
 
 def get_connection():
-    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
